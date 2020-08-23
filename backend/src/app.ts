3736fc95bebcf3192813fa3bdaf75147a8f4ebe8
Robert Lubarski <24817919+lubarskyy@users.noreply.github.com>
2020-08-23 17:22:32 +0200

    feat(core): add postgres database packages, initialize connection upon app start

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Sequelize } from 'sequelize';
 import { Controller } from './interfaces';
 
 export class App {
@@ -12,6 +13,11 @@ export class App {
 
     this.initializeMiddlewares();
     this.initializeControllers(controllers);
+    this.connectToDatabse();
+  }
+
+  public listen(): void {
+    this.app.listen(this.port, () => console.log(`Application initialized and is listening on port ${this.port}.`));
   }
 
   private initializeMiddlewares(): void {
@@ -19,12 +25,26 @@ export class App {
   }
 
   private initializeControllers(controllers: Controller[]): void {
-    controllers.forEach((controller) => {
+    for (const controller of controllers) {
       this.app.use('/api', controller.router);
-    });
+    }
   }
 
-  public listen() {
-    this.app.listen(this.port, () => console.log(`Application initialized and is listening on port ${this.port}`));
+  private async connectToDatabse(): Promise<void> {
+    const sequelize = new Sequelize({
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      dialect: 'postgres',
+    });
+
+    try {
+      await sequelize.authenticate();
+      console.log('Database connection established successfully.');
+    } catch (err) {
+      console.log("Database connection couldn't been established.", err);
+    }
   }
 }
